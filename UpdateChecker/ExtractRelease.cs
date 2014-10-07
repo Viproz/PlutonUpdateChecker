da71c3a89d7bc80dfada5cb4b2d25e6f0fc1c57e
balu92 <balugyanszky.gabor@gmail.com>
2014-10-07 11:55:43 +0200

    download and extract release if needed
    -u, --update stable/latest

---
+++ UpdateChecker/ExtractRelease.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using ICSharpCode.SharpZipLib.Core;
+using ICSharpCode.SharpZipLib.Zip;
+
+namespace UpdateChecker {
+	public class ExtractRelease {
+
+		public static int Extract(string zipFrom) {
+			string pathTo = Environment.CurrentDirectory;
+			ZipFile zipFile = null;
+			try {
+				FileStream fs = File.OpenRead(Path.Combine(Environment.CurrentDirectory, zipFrom));
+				zipFile = new ZipFile(fs);
+				foreach (ZipEntry zEntry in zipFile) {
+					if (!zEntry.IsFile) {
+						continue;
+					}
+					string entryFileName = zEntry.Name;
+
+					byte[] buffer = new byte[4096];
+					Stream zipStream = zipFile.GetInputStream(zEntry);
+
+					string fullZipToPath = Path.Combine(pathTo, entryFileName);
+					string directoryName = Path.GetDirectoryName(fullZipToPath);
+					if (!Directory.Exists(directoryName))
+						Directory.CreateDirectory(directoryName);
+
+					using (FileStream streamWriter = File.Create(fullZipToPath)) {
+						StreamUtils.Copy(zipStream, streamWriter, buffer);
+					}
+				}
+			} catch (Exception ex) {
+				Console.WriteLine(ex.StackTrace);
+				return 2;
+			} finally {
+				if (zipFile != null) {
+					zipFile.IsStreamOwner = true; // Makes close also shut the underlying stream
+					zipFile.Close(); // Ensure we release resources
+				}
+			}
+			return 1;
+		}
+
+		public ExtractRelease () {}
+	}
+}
+
--- UpdateChecker/Main.cs
+++ UpdateChecker/Main.cs
@@ -1,13 +1,45 @@
 using System;
+using System.IO;
+using System.Net;
 
 namespace UpdateChecker {
     class MainClass {
+
+		private static bool update = false;
+
         public static int Main(string[] args) {
             Console.WriteLine("Hello World!");
-			ServerTester tester = new ServerTester();
+			int result = 1;
+
+			foreach (string arg in args) {
+				if (arg.ToLower() == "--update" || arg.ToLower() == "-u") {
+					update = true;
+				}
+				if (update && arg == "stable" || arg == "latest") {
+					string zName = arg + ".zip";
+					using (WebClient Client = new WebClient ())
+					{
+						Client.DownloadFile("https://github.com/Notulp/Pluton/raw/master/Distribution/" + zName, Path.Combine(Environment.CurrentDirectory, zName));
+					}
+
+					System.Threading.Thread.Sleep(250);
+					result = ExtractRelease.Extract(zName);
+					System.Threading.Thread.Sleep(250);
+					if (File.Exists(zName))
+						File.Delete(zName);
+				}
+			}
+
+			new ServerTester();
             Console.ReadKey();
 
-            return 1;
+			return result;
+
+			/* Result:
+			 * 1 - All good
+			 * 2 - Error while extracting the file
+			 * 
+			 */
         }
     }
 }
